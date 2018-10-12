576db9737a16f6b8d24e6ceba40d73d3b651ca3b
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2018-10-12 13:04:17 -0400

    moved everything in render to its own trainFunction

--- pages/index.js
+++ pages/index.js
@@ -5,60 +5,62 @@ import irisTesting from "../data/iris-testing";
 
 class Index extends React.Component {
 
-  render() {
+  componentDidMount() {
+    this.trainFunction();
+  }
+
+  trainFunction = async() => {
     const trainingData = tf.tensor2d(iris.map(item => [
       item.sepal_length, item.sepal_width, item.petal_length, item.petal_width,
   ]))
   
   const outputData = tf.tensor2d(iris.map(item => [
-      item.species === 'setosa' ? 1 : 0,
-      item.species === 'virginica' ? 1 : 0,
-      item.species === 'versicolor' ? 1 : 0,
+    item.species === 'setosa' ? 1 : 0,
+    item.species === 'virginica' ? 1 : 0,
+    item.species === 'versicolor' ? 1 : 0,
   ]))
   
   const testingData = tf.tensor2d(irisTesting.map(item => [
-      item.sepal_length, item.sepal_width, item.petal_length, item.petal_width,
+    item.sepal_length, item.sepal_width, item.petal_length, item.petal_width,
   ]))
   
   const model = tf.sequential()
   
   model.add(tf.layers.dense({
-      inputShape: [4],
-      activation: "sigmoid",
-      units: 5,
+    inputShape: [4],
+    activation: "sigmoid",
+    units: 5,
+  }))
+  
+  model.add(tf.layers.dense({
+    inputShape: [5],
+    activation: "sigmoid",
+    units: 3,
   }))
   
   
   model.add(tf.layers.dense({
-      inputShape: [5],
-      activation: "sigmoid",
-      units: 3,
-  }))
-  
-  
-  model.add(tf.layers.dense({
-      activation: "sigmoid",
-      units: 3,
+    activation: "sigmoid",
+    units: 3,
   }))
   
   model.compile({
-      loss: "meanSquaredError",
-      optimizer: tf.train.adam(0.06)
+    loss: "meanSquaredError",
+    optimizer: tf.train.adam(0.06)
   })
   
   const startTime = Date.now()
-  model.fit(trainingData, outputData, {
-          epochs: 100
-      })
-      .then((history) => {
-      console.log(history);
-          model.predict(testingData).print()
-      })
+  const history = await model.fit(trainingData, outputData, {epochs: 100})
+  console.log(history);
+  const predictions = model.predict(testingData)
+  console.log(`predictions: ${predictions}`);
+  console.log(`irisTesting: ${irisTesting}`);
+  document.getElementById('predictions').innerText += predictions.dataSync();
+  }
 
-    console.log(irisTesting);
-    console.log(history);
+  render() {    
     return (
-      <p></p>
+      <div id = 'predictions'></div>
     );
   }
 }
