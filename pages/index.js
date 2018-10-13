a704806fe3de1435e2fe6aeeef61d25df2b6163e
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2018-10-13 14:53:15 -0400

    added hpjs for #layers and split into train/predict and optfunctions

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,79 @@
 import React from 'react';
+import { Box, Table, TableHeader, TableRow, TableCell, TableBody } from 'grommet';
 import * as tf from '@tensorflow/tfjs';
+import * as hpjs from 'hyperparameters';
 import iris from '../data/iris-training';
 import irisTesting from '../data/iris-testing';
 
+let predictions;
 class Index extends React.Component {
-  componentDidMount() {
-    this.trainFunction();
+  constructor(props) {
+    super(props);
+    this.state = {
+      formattedPredictions: [],
+    };
   }
 
-  trainFunction = async () => {
+
+  componentDidMount() {
+    this.trainAndPredict();
+  }
+
+  arrayto2dArray = (array, size) => {
+    const newArray = [];
+    for (let i = 0; i < array.length; i += size) {
+      newArray.push(array.slice(i, i + size));
+    }
+    this.setState({
+      predictions: newArray, // eslint-disable-line react/no-unused-state
+    });
+  };
+
+
+  optFunction = async ({ numLayers }, { trainingData, outputData }) => {
+    const model = tf.sequential();
+
+    model.add(tf.layers.dense({
+      inputShape: [4],
+      activation: 'sigmoid',
+      units: 5,
+    }));
+
+    console.log(numLayers);
+    for (let i = 0; i < numLayers; i += 1) {
+      model.add(tf.layers.dense({
+        activation: 'sigmoid',
+        units: 3,
+      }));
+    }
+
+    model.add(tf.layers.dense({
+      activation: 'sigmoid',
+      units: 3,
+    }));
+
+    model.compile({
+      loss: 'meanSquaredError',
+      optimizer: tf.train.adam(0.06),
+    });
+
+    const h = await model.fit(trainingData, outputData, { epochs: 100 });
+
+    // return the model, loss, and status
+    return {
+      model,
+      loss: h.history.loss[h.history.loss.length - 1],
+      status: hpjs.STATUS_OK,
+    };
+  };
+
+
+  trainAndPredict = async () => {
+    const space = {
+      numLayers: hpjs.quniform(1, 5, 1),
+    };
+
+
     const trainingData = tf.tensor2d(iris.map(item => [
       item.sepal_length, item.sepal_width, item.petal_length, item.petal_width,
     ]));
@@ -23,42 +88,54 @@ class Index extends React.Component {
       item.sepal_length, item.sepal_width, item.petal_length, item.petal_width,
     ]));
 
-    const model = tf.sequential();
+    const trials = await hpjs.fmin(
+      this.optFunction, space, hpjs.search.randomSearch, 3,
+      { rng: new hpjs.RandomState(654321), trainingData, outputData }
+    );
 
-    model.add(tf.layers.dense({
-      inputShape: [4],
-      activation: 'sigmoid',
-      units: 5,
-    }));
+    const opt = trials.argmin;
 
-    model.add(tf.layers.dense({
-      inputShape: [5],
-      activation: 'sigmoid',
-      units: 3,
-    }));
+    console.log(opt);
 
+    const { model } = await this.optFunction(opt, { trainingData, outputData });
 
-    model.add(tf.layers.dense({
-      activation: 'sigmoid',
-      units: 3,
-    }));
+    predictions = model.predict(testingData).dataSync();
 
-    model.compile({
-      loss: 'meanSquaredError',
-      optimizer: tf.train.adam(0.06),
+    const formattedArray = [];
+    for (let i = 0; i < predictions.length; i += 3) {
+      formattedArray.push(predictions.slice(i, i + 3));
+    }
+    this.setState({
+      formattedPredictions: formattedArray, // eslint-disable-line react/no-unused-state
     });
 
-    const history = await model.fit(trainingData, outputData, { epochs: 100 });
-    console.log(history);
-    const predictions = model.predict(testingData);
-    console.log(`predictions: ${predictions}`);
-    console.log(`irisTesting: ${irisTesting}`);
-    document.getElementById('predictions').innerText += predictions.dataSync();
+    // console.log(`predictions: ${predictions}`);
+    // console.log(`irisTesting: ${irisTesting}`);
   }
 
   render() {
+    const { formattedPredictions } = this.state;
     return (
-      <div id='predictions' />
+      <Box>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableCell size='xxsmall' scope='col' border='bottom'><b>Setosa</b></TableCell>
+              <TableCell size='xxsmall' scope='col' border='bottom'><b>Virginica</b></TableCell>
+              <TableCell size='xxsmall' scope='col' border='bottom'><b>Versicolor</b></TableCell>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {formattedPredictions.map((e, index) => (
+              <TableRow key={`predictions_${index}`}>
+                <TableCell size='xxsmall' scope='row'>{e[0]}</TableCell>
+                <TableCell size='xxsmall' scope='row'>{e[1]}</TableCell>
+                <TableCell size='xxsmall' scope='row'>{e[2]}</TableCell>
+              </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </Box>
     );
   }
 }
